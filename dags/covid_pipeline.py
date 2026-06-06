c9874b23317d58bff83a4bf2ce05f562b0fa39fd
Amir Nassar <amirabdelgafar456@gmail.com>
2026-06-07 00:43:41 +0300

    Build the Airflow 3.x DAG

--- dags/covid_pipeline.py
+++ dags/covid_pipeline.py
@@ -1,47 +1,43 @@
-from airflow import DAG
-from airflow.operators.python import PythonOperator
-from datetime import datetime, date
+from airflow.sdk import dag, task
+from pendulum import datetime
 import sys
 
-sys.path.append("/opt/airflow/scripts")
+if "/opt/airflow/scripts" not in sys.path:
+    sys.path.append("/opt/airflow/scripts")
 
 from bronze import ingest_one_day
 from silver import bronze_to_silver
 from gold   import silver_to_gold
 
 
-def bronze_task(**context):
-    # Airflow 2.x: use execution_date instead of logical_date
-    execution_date = context["execution_date"].date()
-    ingest_one_day(execution_date)
-
-
-with DAG(
+@dag(
     dag_id="covid_daily_bronze",
-    start_date=datetime(2020, 1, 22),
-    schedule_interval="@daily",
-    catchup=True,      # backfill all past dates
-    max_active_runs=3, # run 3 dates in parallel
-    tags=["covid", "bronze"],
-) as dag:
+    start_date=datetime(2020, 1, 22, tz="UTC"),
+    end_date=datetime(2023, 3, 9, tz="UTC"),
+    schedule="@daily",
+    catchup=True,
+    max_active_runs=3,
+    tags=["covid", "bronze", "silver", "gold"],
+)
+def covid_pipeline():
 
-    bronze = PythonOperator(
-        task_id="ingest_bronze",
-        python_callable=bronze_task,
-    )
+    @task
+    def bronze_task(logical_date=None):
+        ingest_one_day(logical_date.date())
 
-    silver = PythonOperator(
-        task_id="clean_silver",
-        python_callable=bronze_to_silver,
-    )
+    @task
+    def silver_task():
+        bronze_to_silver()
 
-    gold = PythonOperator(
-        task_id="model_gold",
-        python_callable=silver_to_gold,
-    )
+    @task
+    def gold_task():
+        silver_to_gold()
+
+    bronze = bronze_task()
+    silver = silver_task()
+    gold   = gold_task()
 
     bronze >> silver >> gold
 
 
-
-
+covid_pipeline()
